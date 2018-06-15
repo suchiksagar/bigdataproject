820d701bbee9243694629adc7796b6e32822a89a
Sucharith Kshirasagar <skshiras@mathworks.com>
2018-06-14 23:36:15 -0400

    added s3 sink

--- flume/bootstrap.sh
+++ flume/bootstrap.sh
@@ -6,6 +6,7 @@ while :
 do
 	TMS=$(date +%s)
 	curl http://$SERVICE_HOST:8090/filepollws/v1/filepoll > /tmp/dump/data-dump-$TMS.json
+	curl -X POST -H "Content-Type: text/plain" --data "$(</tmp/dump/data-dump-$TMS.json)" http://$SINK_HOST:7075/httpsink/v1/publish
 	sleep 12
 	mv /tmp/dump/data-dump-$TMS.json /tmp/flumefeed/data-dump-$TMS.json
 done
