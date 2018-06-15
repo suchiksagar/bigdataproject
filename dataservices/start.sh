d099b9abc6429dba75cb0481d8bcf020ee5d11f8
suchiksagar <sagarksuchi@gmail.com>
2018-06-15 08:19:25 -0400

    added clean up of data dump: avoids cluttering the system

--- dataservices/start.sh
+++ dataservices/start.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 java -jar /tmp/bigdata/filepollws/app/filepollws-1.0.0-SNAPSHOT.jar &
-java -jar /tmp/bigdata/weatherdataws/app/weatherdataws-1.0.0-SNAPSHOT.jar --APIKEY=$APIKEY
+java -jar /tmp/bigdata/weatherdataws/app/weatherdataws-1.0.0-SNAPSHOT.jar --APIKEY=$APIKEY &
+while :
+do
+    sleep 120 && find /tmp/bigdata/weatherdata/dump -mmin +10 -type f -exec rm -fv {} \; > rmfiles.log
+done
