1d056a9d931de39094a25203212a93d486e1d541
Sucharith Kshirasagar <skshiras@mathworks.com>
2018-06-19 15:07:22 -0400

    added ELASTIC_HOST env variable

--- weatherkafkaconsumer/bootstrap.sh
+++ weatherkafkaconsumer/bootstrap.sh
@@ -2,4 +2,4 @@
 #run the kafka console util to create topic
 /tmp/downloads/kafka/bin/kafka-topics.sh --create --zookeeper ${DOCKER_HOST}:2181 --replication-factor 1 --partitions 2 --topic ${KAFKA_TOPIC}
 echo "Created Topic, will kick off consumer"
-java -jar /opt/kafkaconsumer/weatherkafkaconsumer.jar --kafka.bootstrapAddress=${DOCKER_HOST} --elastic.bootstrapAddress=${DOCKER_HOST} --userlog.topic.name=${KAFKA_TOPIC}
+java -jar /opt/kafkaconsumer/weatherkafkaconsumer.jar --kafka.bootstrapAddress=${DOCKER_HOST} --elastic.bootstrapAddress=${ELASTIC_HOST} --userlog.topic.name=${KAFKA_TOPIC}
